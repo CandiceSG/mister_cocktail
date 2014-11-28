b61e3a75c74a54e3d52339bb4ef7e02d26455944
Thomas Gautier <thomas.gautier@gmail.com>
2014-11-28 16:56:45 +0100

    Add paperclip and bootstrap sexyness

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
 class Cocktail < ActiveRecord::Base
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 
-  validates_presence_of :name
+  validates_presence_of :name, :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
